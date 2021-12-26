8857684e55bf921c3bd7fb846d5ec40a92a977e4
Jesus Suero <37484153+jesuero@users.noreply.github.com>
2021-12-26 22:09:56 +0100

    typo

--- Session-13-BORME/borme/__main__.py
+++ Session-13-BORME/borme/__main__.py
@@ -8,7 +8,7 @@ from os.path import exists
 import shutil
 
 '''
-This script navigate through https://www.boe.es/diario_borme/ website and find specific sections 
+This script navigates through https://www.boe.es/diario_borme/ website and find specific sections 
 that correspond to a specific date to download pdf and xml files sorted in directories. 
 This is done using requests, Selenium, Chromedriver and BeautifulSoup.
 '''
